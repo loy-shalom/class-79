111959216c4853467aa15cf5da32e9b8ee63d226
loy-shalom <93376919+loy-shalom@users.noreply.github.com>
2022-04-09 08:18:09 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,23 +2,46 @@ student_name=[];
 
 function submit()
 {
-    var name1=document.getElementById("n1").value;
-    var name2=document.getElementById("n2").value;
-    var name3=document.getElementById("n3").value;
-    var name4=document.getElementById("n4").value;
-
-    student_name.push(name1);
-    student_name.push(name2);
-    student_name.push(name3);
-    student_name.push(name4);
-
+    var display_student_array=[];
+    for(var j=1;j<=4;j++)
+    {
+        var name_student=document.getElementById("n"+j).value;
+        console.log(name_student);
+        student_name.push(name_student);
+    }
     console.log(student_name);
-    document.getElementById("display_name").innerHTML=student_name;
+    var length_array=student_name.length;
+    console.log(length_array);
+
+    for(var k=0;k<length_array;k++)
+    {
+        display_student_array.push("<h4>NAME - "+ student_name[k] + "</h4>");
+        console.log(display_student_array);
+    }
+    console.log(display_student_array);
+
+    document.getElementById("display_name_with_commas").innerHTML=display_student_array;
+
+    var remove_commas=display_student_array.join(" ");
+    console.log(remove_commas);
+    document.getElementById("display_name_without_commas").innerHTML=remove_commas;
+
     document.getElementById("submit_button").style.display="none";
     document.getElementById("sort_button").style.display="inline-block";
 }
 function sort(){
     student_name.sort();
     console.log(student_name);
-    document.getElementById("display_name").innerHTML=student_name;
+    var display_student_array_sort=[];
+    var length_array=student_name.length;
+    console.log(length_array);
+    for(var k=0;k<length_array;k++)
+    {
+        display_student_array_sort.push("<h4>NAME - "+ student_name[k] + "</h4>");
+        console.log(display_student_array_sort);
+    }
+    var remove_commas=display_student_array_sort.join(" ");
+    console.log(remove_commas);
+    document.getElementById("display_name_without_commas").innerHTML=remove_commas;
+    
 }
